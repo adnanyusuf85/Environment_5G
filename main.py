76667709ad97bf8643c2adb60cb4df84e4875794
Adnan Yusuf <adnanyusuf@outlook.com>
2025-10-17 07:28:35 +0500

    Add entry point code

--- main.py
+++ main.py
@@ -1,5 +1,48 @@
 import networkx as nx
+from heapq import heapify
 
-G = nx.grid_2d_graph(5,5)
-print(G.nodes)
+def add_traffic_signals(graph, traffic_signals):
+    graph.add_node((5,0))
 
+def add_autonomous_vehicles(graph, autonomous_vehicles):
+    pass
+
+def add_pedestrians(graph, pedestrians):
+    pass
+
+def assign_destinations(autonomous_vehicles):
+    pass
+
+
+class Simulator:
+
+    def __init__():
+        self.event_queue = []
+
+    def load_environment(roadspace_environment):
+        vehicles_events(world_environment.network_users)
+        rf_propagation_events(world_environment.roadspaces)
+        road_events(world.environment.roadspaces)
+        sector_events()
+
+    def initialize_entities():
+        pass
+
+    def step():
+        heapify(self.event_queue)
+
+    def enlist_publisher
+
+
+
+roadspace_environment = None
+roadspace_environment.add_traffic_signals(traffic_signals)
+roadspace_environment.add_autonomous_vehicles(autonomous_vehicles)
+roadspace_environment.add_pedestrians(pedestrians)
+roadspace_environment.add_gnbs(gnbs)
+
+
+simulator.add_environment(roadspace_environment)
+
+while (True):
+    simulation_step(None)
